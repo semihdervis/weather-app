c88cef9f9a626587e56ef4cb63db9f8e6cd3bb93
semihdervis <semihdarwish@gmail.com>
2024-03-11 13:15:09 +0300

    add templates

--- main.py
+++ main.py
@@ -1,55 +1,44 @@
+from flask import Flask, render_template, request
+import requests
 from dotenv import load_dotenv
 import os
-import requests
-import json
 
-# Load variables from .env file
 load_dotenv()
 
-# Access api key in .env
-api_key = os.getenv("API_KEY")
+app = Flask(__name__)
 
-icon_mapping = {
-    '01d': '☀️',  # clear sky day
-    '02d': '⛅',  # few clouds day
-    '03d': '☁️',  # scattered clouds day
-    '04d': '☁️',  # broken clouds day
-    '09d': '🌧️',  # shower rain day
-    '10d': '🌦️',  # rain day
-    '11d': '⛈️',  # thunderstorm day
-    '13d': '❄️',  # snow day
-    '50d': '🌫️',  # mist day
-    # Add night variants if needed
-}
+# Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+API_KEY = os.getenv("API_KEY")
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
-def get_weather(api_key, city):
-    # OpenWeather API endpoint
-    endpoint = "http://api.openweathermap.org/data/2.5/weather"
 
-    # Parameters for the API request
-    params = {
-        'q': city,
-        'appid': api_key,
-        'units': 'metric'  # You can change this to 'imperial' for Fahrenheit
-    }
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    weather_data = None
 
-    # Make the API request
-    response = requests.get(endpoint, params=params)
+    if request.method == 'POST':
+        city = request.form['city']
+        weather_data = get_weather_data(city)
 
-    # Check if the request was successful (status code 200)
+    return render_template('index.html', weather_data=weather_data)
+
+
+def get_weather_data(city):
+    params = {'q': city, 'appid': API_KEY, 'units': 'metric'}
+    response = requests.get(BASE_URL, params=params)
+
     if response.status_code == 200:
-        # Parse the JSON data from the response
         data = response.json()
+        weather = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+        }
+        return weather
 
-        # Print relevant weather information
-        print(f"Weather in {city}, {data['sys']['country']}, {data['weather'][0]['icon']}, {icon_mapping.get(data['weather'][0]['icon'], '❓')}:")  # Add country information
-        print(f"Temperature: {data['main']['temp']}°C")
-        print(f"Description: {data['weather'][0]['description']}")
-    else:
-        # Print an error message if the request was not successful
-        print(f"Error: Unable to fetch weather data. Status code: {response.status_code}")
+    return None
 
-if __name__ == "__main__":
-    city = 'Istanbul'  # Replace with the name of the city you want to get the weather for
 
-    get_weather(api_key, city)
+if __name__ == '__main__':
+    app.run(debug=True)
